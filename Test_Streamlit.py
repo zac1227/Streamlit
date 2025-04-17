7ca2e08580774a3646d628feaa694b278333a6fb
zac1227 <94905993+zac1227@users.noreply.github.com>
2025-04-17 18:39:31 +0800

    Update Test_Streamlit.py

--- Test_Streamlit.py
+++ Test_Streamlit.py
@@ -61,9 +61,9 @@ def show_predict_page():
     g_hei = st.sidebar.number_input(label='Height', value=180.0, step=0.1)
     g_wei = st.sidebar.number_input(label='Weight', value=40.0, min_value=40.0, step=0.1)
     lf_ldh = st.sidebar.number_input(label='LDH', value=0.0, step=0.1)
-    cbc_leu = st.sidebar.number_input(label='cbc_leu', value=0.0,  step=0.1)
-    systolic = st.sidebar.number_input(label='systolic', value=0.0,  step=0.1)
-    lf_tb = st.sidebar.number_input(label='lf_tb', value=0.0,  step=0.1)
+    #cbc_leu = st.sidebar.number_input(label='cbc_leu', value=0.0,  step=0.1)
+    #systolic = st.sidebar.number_input(label='systolic', value=0.0,  step=0.1)
+    #lf_tb = st.sidebar.number_input(label='lf_tb', value=0.0,  step=0.1)
     
     
 
